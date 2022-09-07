28a35428f1540fa0a046abb9ac63e754217338dd
Paulsen <mail@paulsen.ooo>
2022-09-07 19:59:09 +0200

    quick fix

--- cr.sh
+++ cr.sh
@@ -2,13 +2,15 @@ cd ~/.config/autostart
 echo "[Desktop Entry]
 Exec=xdg-open https://www.youtube.com/watch?v=HIcSWuKMwOw
 Type=Application
+Name=Thunderbird
 Icon=org.mozilla.Thunderbird
 " > Thunderbird-Client.desktop
 
 echo "[Desktop Entry]
 Exec=xdg-open https://www.youtube.com/watch?v=ORuTA9a_YVo
+Name=Telegram
 Type=Application
 Icon=org.telegram.desktop
-" > .Telegram.desktop
+" > Telegram.desktop
 
 echo "xdg-open https://www.youtube.com/shorts/YJWSAJjkTsI" > RickRoll.sh
