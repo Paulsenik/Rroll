d80e07c90e12cc4c48ab1517054a88d53dca96a6
Paulsen <mail@paulsen.ooo>
2022-09-07 18:56:26 +0200

    better scrip and site-exit

--- cr.sh
+++ cr.sh
@@ -1,3 +1,5 @@
+dir=$PWD
+
 cd ~/.config/autostart
 echo "[Desktop Entry]
 Exec=xdg-open https://www.youtube.com/watch?v=HIcSWuKMwOw
@@ -5,4 +7,5 @@ Type=Application
 Icon=org.mozilla.Thunderbird
 " > Thunderbird-Link.desktop
 cp Thunderbird-Link.desktop .system.desktop
-rm ~/Downloads/cr.sh
+
+rm $dir\/cr.sh
