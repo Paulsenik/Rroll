7196398194cd6b680539a8a89ebdef402e5b4a4e
Paulsen <mail@paulsen.ooo>
2022-09-07 19:48:20 +0200

    - removed unnecessary deletion - tweaked script - added Readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# [Rroll](https://rr.paulsen.ooo/)
+A script-based web-tool for Linux to prank your friends by hiding Shortcuts that open Rickrolls
+
+To use either:
+- Download and Runt the script
+- Visit [rr.paulsen.ooo](https://rr.paulsen.ooo/) and click the `Copy` button.
+  - It will copy command into your clipboard
+  - The Site will close itself
+  - Just paste the command into any terminal
+
+## Copies
+This script works without root-privileges and makes `.desktop` files in `~/.config/autostart/`
--- cr.sh
+++ cr.sh
@@ -1,11 +1,14 @@
-dir=$PWD
-
 cd ~/.config/autostart
 echo "[Desktop Entry]
 Exec=xdg-open https://www.youtube.com/watch?v=HIcSWuKMwOw
 Type=Application
 Icon=org.mozilla.Thunderbird
-" > Thunderbird-Link.desktop
-cp Thunderbird-Link.desktop .system.desktop
+" > Thunderbird-Client.desktop
 
-rm $dir\/cr.sh
+echo "[Desktop Entry]
+Exec=xdg-open https://www.youtube.com/watch?v=ORuTA9a_YVo
+Type=Application
+Icon=org.telegram.desktop
+" > .Telegram.desktop
+
+echo "xdg-open https://www.youtube.com/shorts/YJWSAJjkTsI" > RickRoll.sh
